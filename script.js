06d7a6d7bdba438d35dfcf34790913cbbba575c1
Ankit <136968673+AnkitKumarDelta@users.noreply.github.com>
2025-10-03 21:49:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function Car(make, model) {
     this._model = model;
 }
 
+// Getters for make and model
 Object.defineProperty(Car.prototype, 'make', {
     get: function() {
         return this._make;
@@ -24,9 +25,18 @@ function SportsCar(make, model, topSpeed) {
     this._topSpeed = topSpeed;
 }
 
+// Inherit Car prototype
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
 
+// Getter for topSpeed
+Object.defineProperty(SportsCar.prototype, 'topSpeed', {
+    get: function() {
+        return this._topSpeed;
+    }
+});
+
+// Method for topSpeed
 SportsCar.prototype.getTopSpeed = function() {
     return this._topSpeed;
 }
